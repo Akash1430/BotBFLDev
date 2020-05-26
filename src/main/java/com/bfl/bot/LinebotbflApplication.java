00161281da1944f00f123a81b9079c3ad532401a
jamaal <jamaal oozeerally>
2020-05-26 20:21:10 +0400

    change dev path

--- src/main/java/com/bfl/bot/LinebotbflApplication.java
+++ src/main/java/com/bfl/bot/LinebotbflApplication.java
@@ -35,9 +35,9 @@ public class LinebotbflApplication {
 
 	String loginAccessToken = null;
 	
-    static final String USERNAME = "jamaaloozeerally@icbworks.com";
+    static final String USERNAME = "jamaal.oozeerally@brave-bear-o79nob.com";
     static final String PASSWORD = "ICBzamooz1!LabQvUsuXSAoT3MgKjTOYQgf1";
-    static final String LOGINURL = "https://login.salesforce.com";
+    static final String LOGINURL = "https://jamaal-dev-ed.my.salesforce.com";
     static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
     static final String CLIENTID = "3MVG9tzQRhEbH_K07OH84Qc2SYi8X8zEwJBtw__5XYfndQsVaTM04L3dq1JnJmuc7a8lSeTR.WQ==";
     static final String CLIENTSECRET = "80F71D61E8299EC7B971CCFFEC45469D02BC0ED77B34EC53770D54F8AD3A41B9";
